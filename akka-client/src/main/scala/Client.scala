b18d303b5bcda8f0dadb76a8899b2920d48ee963
tushar686 <tushar686@gmail.com>
2014-07-03 12:07:08 +0530

    removing test

--- akka-client/src/main/scala/Client.scala
+++ akka-client/src/main/scala/Client.scala
@@ -4,10 +4,6 @@ import akka.actor._
 import akka.actor.ActorDSL._
 
  object Client_Sender extends App {
-   implicit val system = ActorSystem("Client")
-
-   val server = system.actorSelection("akka.tcp://Server@10.198.80.147:2552/user/server")
-
    /*val a = actor(new Act {
       whenStarting {
         for(i <- 1 to 10000) yield {server ! "msg= " + i + ":Zeeshan-10.198.81.62:Rupin-10.198.81.142:Priti-10.198.81.152" } 
@@ -15,6 +11,9 @@ import akka.actor.ActorDSL._
    })*/
    
    def floatMeInTheRing(msg: String) = {
+   implicit val system = ActorSystem("Client")
+   val server = system.actorSelection("akka.tcp://Server@10.198.80.147:2552/user/server")
+   println(server)
      server ! msg
    }
 }
