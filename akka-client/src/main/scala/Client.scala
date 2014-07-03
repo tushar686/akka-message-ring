7abcede95a895d6d1bbd586054e8c41d656e6c26
tushar686 <tushar686@gmail.com>
2014-07-03 14:46:11 +0530

    dynamic root

--- akka-client/src/main/scala/Client.scala
+++ akka-client/src/main/scala/Client.scala
@@ -2,18 +2,27 @@ package client
 
 import akka.actor._
 import akka.actor.ActorDSL._
+import scala.concurrent.Future
 
  object Client_Sender extends App {
-   /*val a = actor(new Act {
+	implicit val system = ActorSystem("Client")
+	val root = "10.198.80.147"
+   val a = actor(new Act {
       whenStarting {
-        for(i <- 1 to 10000) yield {server ! "msg= " + i + ":Zeeshan-10.198.81.62:Rupin-10.198.81.142:Priti-10.198.81.152" } 
+        val server = system.actorSelection("akka.tcp://Server@"+ root +":2552/user/server")
+       	(1 to 10).par foreach {i=> Future {server ! "msg= " + i + "root-" + root + ":Zeeshan-10.198.81.63:Rupin-10.198.80.73:Priti-10.198.81.132"}(Config.executionContext) } 
       }     
-   })*/
-   
-   def floatMeInTheRing(msg: String) = {
-   implicit val system = ActorSystem("Client")
-   val server = system.actorSelection("akka.tcp://Server@10.198.80.147:2552/user/server")
-   println(server)
-     server ! msg
-   }
+   })
+}
+
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContextExecutorService
+
+object Config extends Config(numberOfthreads = 200)
+
+class Config(numberOfthreads: Int) {
+  val threadPool:ExecutorService = Executors.newScheduledThreadPool(numberOfthreads)
+  implicit val executionContext: ExecutionContextExecutorService = ExecutionContext.fromExecutorService(threadPool)
 }
