ee7492f4dcb42a43e43afbb78eb453410be7c10d
tushar686 <tushar686@gmail.com>
2014-07-07 17:54:18 +0530

    added mailbox capacity and dispatcher

--- akka-server/src/main/scala/Server.scala
+++ akka-server/src/main/scala/Server.scala
@@ -2,16 +2,21 @@ import akka.actor.Actor
 import akka.actor.ActorLogging
 import akka.actor.ActorSystem
 import akka.actor.Props
+import akka.dispatch._
 
-class Server extends Actor {
+class Server extends Actor with RequiresMessageQueue[BoundedMessageQueueSemantics] {
+  var total_messages = 0;
+  val IP_DELIMITER = ","
   def receive = {
-    case msg: String => println("########################received " + msg)
+    case msg: String => total_messages = total_messages + 1
+      					println("\n########################received " + msg)
+      					println("\ntotal= " + total_messages)
       					sendMessageToNextMachineInTheRing(msg)    					
-    case _ => println("Received unknown msg")
+    case _ => println("\nReceived unknown msg")
   }
   
   def sendMessageToNextMachineInTheRing(msg: String) = {
-    val msgList = msg.split(":").toList
+    val msgList = msg.split(IP_DELIMITER).toList
     msgList match {
       case _ if msgList.size == 1 => sendMessageBackToRoot(msgList) 
       case _ if msgList.size > 1 =>  sendMessageToNextIp(msgList)
@@ -22,29 +27,30 @@ class Server extends Actor {
   def sendMessageToNextIp(msgList: List[String]) = {
     val ips = msgList.tail
     println("************************forwarding to " + ips.head)
-	 val server = Server.system.actorSelection("akka.tcp://Server@" + ips.head.split("-")(1) + ":2552/user/server")
+	 val server = Server.system.actorSelection("akka.tcp://Server@" + ips.head.split("-")(1) + "/user/server")
 	 ips match {
 		case _ if ips.size == 1 =>
 		  server ! msgList.head
 		case _ if ips.size > 1 =>
-		  server ! msgList.head + ":" + ips.tail.mkString(":")
+		  server ! msgList.head + IP_DELIMITER + ips.tail.mkString(IP_DELIMITER)
 	 }
   }
   
   def sendMessageBackToRoot(msgList: List[String]) = {
 	 if(!msgList.head.contains("back")) {
 	   val rootIp = msgList.head.split("root").toList.last
-	   val server = Server.system.actorSelection("akka.tcp://Server@" + rootIp +":2552/user/server")
-	   println("========================sending back to Root for Ring Conpletion")
+	   val server = Server.system.actorSelection("akka.tcp://Server@" + rootIp +"/user/server")
+	   println("\n========================sending back to Root for Ring Conpletion")
 	   server ! msgList.head + " back"
 	 }
   }
   	
 }
 
+
 object Server extends App {
   val system = ActorSystem("Server")
-  val server = system.actorOf(Props[Server], name = "server")
+  val server = system.actorOf(Props[Server].withDispatcher("server-dispatcher"), name = "server")
   server ! "Test back message"
   println("Server ready")
 }
